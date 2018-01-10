45518bd7095ccd2a7428ee90503af7a24e350d48
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-10 16:50:29 +0000

    Customer can select an item from a menu.

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,17 @@
+require_relative 'menu'
 # Order class
 class Order
+  attr_reader :basket, :selected_item
   def initialize
     @menu = Menu.new
+    @basket = []
   end
 
-  def select_item(item_num)
-    @menu.display_items[item_num]
+  def select_item(menu_category, item)
+    category = @menu.current_menu[menu_category]
+    selection = category.each do |dish|
+      return dish if dish['name'] == item
+    end
+    p @selected_item = selection
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,10 +2,15 @@ require 'order'
 
 describe Order do
   let(:order) { Order.new }
-  let(:menu) { double :menu, name: 'Sweets and hot drinks' }
 
   it 'lets you select an item from the menu' do
-    # allow(menu).to receive(:display_items).and_return(['Cake, 5.60'])
-    expect(order.select_item(0)).to eq 'Cake, 5.60'
+    expect(order.select_item('barbecue-menu', 'Ribs'))
+      .to eq('name' => 'Ribs', 'price' => 2.50)
+  end
+
+
+  xit 'lets you order a quantity of a chosen item' do
+    order.place_order(0, 2)
+    expect(order.basket).to eq ['Cake, 5.60', 'Cake, 5.60']
   end
 end
