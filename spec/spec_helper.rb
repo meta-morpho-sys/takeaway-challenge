3c3357dc40bd02e03f0038ce89e7910608f0ab2e
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-11 00:04:20 +0000

    Configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SCF = SimpleCov::Formatter
+formatters = [SCF::Console, SCF::HTMLFormatter]
+SimpleCov.formatter = SCF::MultiFormatter.new(formatters)
+
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
