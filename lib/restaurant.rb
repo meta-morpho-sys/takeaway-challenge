34c3c95771d5291cb678cc1579a047b860e869e9
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-13 15:50:07 +0000

    Introduced class Restaurand which will be the center of handling menus, orders and confirmations. First Feature - display and choose a menu

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,12 +1,30 @@
-# Handles sending of messages with Twillio
-class Takeaway
+require 'menu'
+# Shows menu, makes orders and handles sending of confirmation
+# messages with Twilio
+class Restaurant
+  def initialize
+    @menu = Menu.new
+  end
+
+  # Displays only the categories(titles) of existing menus.
+  def display_menus
+    @menu.current_menu.each_key do |category|
+      p category
+    end
+  end
+
+  # Displays the details of the chosen menu category.
+  def choose_menu(category)
+    @menu.display_items(category)
+  end
 
   def complete_order(total_price)
     send_text("Thank you for your order: £#{total_price}")
   end
 
   def send_text(message)
-    Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'],
+                             ENV['TWILIO_AUTH_TOKEN'])
                         .messages.create(
                           from: ENV['TWILIO_PHONE'],
                           to: ENV['TWILIO_DESTINATION_PHONE'],
