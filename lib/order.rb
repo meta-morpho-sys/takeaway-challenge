0415461c0ddd846f09177ac3a846cbacc994bffc
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-15 16:39:33 +0000

    Code and specs updated for the use of Money gem; time is stubbed and tested with Timecop gem.

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,7 @@
 require_relative 'menu'
 require_relative 'messenger'
 require 'twilio-ruby'
+require 'money'
 
 # Defining helper classes which then will be instantiated where necessary.
 # Such classes are perfect for storing data.
@@ -21,11 +22,11 @@ class Order
     category = @menu.current_menu[menu_category]
     category.each do |dish|
       if dish['name'] == item_name
-        item = Item.new(dish['name'], dish['price'])
+        item = Item.new(dish['name'], Money.from_amount(dish['price'], 'GBP'))
         return item
       end
     end
-    raise 'Item not found in this menu.'
+    raise 'Item not found on this menu.'
   end
 
   # Returns an array of Entries containing items
@@ -34,6 +35,8 @@ class Order
     selected_item = select_item(menu_category, item_name)
     entry = Entry.new(selected_item, quantity)
     @basket << entry
+    p "#{entry.quantity} x #{entry.item.name}/s added to your basket"\
+    " at #{entry.item.price} each"
   end
 
   def total
@@ -42,8 +45,19 @@ class Order
     end
   end
 
-  def complete_order(total_price)
-    send_text("Thank you for your order: £#{total_price}")
+  def checkout(total_price)
+    raise 'incorrect price' unless correct_amount?(total_price)
+    time = Time.new
+    delivery_time = (time + 3600).strftime('%H:%M')
+    send_text("Thank you! Your order : £#{total_price} was placed and will be"\
+    " delivered before #{delivery_time}.")
+  end
+
+  private
+
+  def correct_amount?(total_price)
+    price_to_compare = total
+    total_price == price_to_compare
   end
 
   def send_text(message)
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,12 @@
 require 'order'
+require 'timecop'
 
 describe Order do
   let(:order) { Order.new }
 
   it 'lets you select an item from the menu' do
     expect(order.select_item('barbecue', 'Ribs'))
-      .to eq(Item.new('Ribs', 2.50))
+      .to eq(Item.new('Ribs', Money.new(250, 'GBP')))
   end
 
   it 'raises an exception if an item is not found' do
@@ -14,7 +15,7 @@ describe Order do
   end
 
   it 'lets you order a quantity of a chosen item' do
-    item = Item.new('Ice Cream', 1.5)
+    item = Item.new('Ice Cream', Money.new(150, 'GBP'))
     QUANTITY = 2
     entry = Entry.new(item, QUANTITY)
     basket = [entry]
@@ -25,18 +26,30 @@ describe Order do
 
   it 'gives the total of one entry' do
     order.add_item('drinks', 'Apple Juice', 2)
-    expect(order.total).to eq 4.60
+    expect(order.total).to eq Money.new(460, 'GBP')
   end
 
   it 'gives the total of more entries' do
     order.add_item('drinks', 'Apple Juice', 2)
     order.add_item('barbecue', 'Ribs', 2)
-    expect(order.total).to eq 9.60
+    expect(order.total).to eq Money.new(960, 'GBP')
+  end
+
+  it 'checks that the total given to the customer is correct' do
+    order.add_item('drinks', 'Apple Juice', 2)
+    order.add_item('barbecue', 'Ribs', 2)
+    expect { order.checkout(Money.new(550, 'GBP')) }
+      .to raise_exception 'incorrect price'
   end
 
   # Stubbing Twilio
   it 'sends a payment confirmation text message' do
-    expect(order).to receive(:send_text).with('Thank you for your order: £9.95')
-    order.complete_order(9.95)
+    frozen_time = Timecop.freeze(Time.now)
+    message = 'Thank you! Your order : £5.00 was placed and will be'\
+    " delivered before #{(frozen_time + 3600).strftime('%H:%M')}."
+
+    order.add_item('barbecue', 'Ribs', 2)
+    expect(order).to receive(:send_text).with(message)
+    order.checkout(Money.new(500, 'GBP'))
   end
 end
